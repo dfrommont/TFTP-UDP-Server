b9c7f359cbe77781bd224fcc329e560087bbc223
David Frommont <davidfrommont@gmail.com>
2024-04-08 11:08:31 +0100

    updated code for two-way communication

--- src/main/java/Server/UDPSocketServer.java
+++ src/main/java/Server/UDPSocketServer.java
@@ -1,17 +1,11 @@
 package Server;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.util.Date;
-import java.util.Objects;
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
 
 public class UDPSocketServer extends Thread {
-
-    protected DatagramSocket socket = null;
+    private static final int MAX_PACKET_SIZE = 10000; // Maximum size of UDP packet
+    protected DatagramSocket socket;
 
     public UDPSocketServer() throws SocketException {
         this("UDPSocketServer");
@@ -19,63 +13,97 @@ public class UDPSocketServer extends Thread {
 
     public UDPSocketServer(String name) throws SocketException {
         super(name);
-        // **********************************************
-        // Add a line here to instantiate a DatagramSocket for the socket field defined above.
-        // Bind the socket to port 9000 (any port over 1024 would be ok as long as no other application uses it).
-        // Ports below 1024 require administrative rights when running the applications.
-        // Take a note of the port as the client needs to send its datagram to an IP address and port to which this server socket is bound.
-        //***********************************************
-
-        socket = new DatagramSocket(1234);
+        socket = new DatagramSocket(9000);
 
     }
 
-    @Override
-    public void run() {
-
-        byte[] recvBuf = new byte[256];     // a byte array that will store the data received by the client
+    @Override public void run() {
+        DatagramSocket serverSocket = null;
 
         try {
-            DatagramPacket packet = new DatagramPacket(recvBuf, 256);
-            socket.receive(packet);
+            // Create a UDP socket
+            serverSocket = new DatagramSocket(1234);
 
-            String dString = new Date().toString();
-            int len = dString.length();                                             // length of the byte array
-            byte[] buf = new byte[len];                                             // byte array that will store the data to be sent back to the client
-            System.arraycopy(dString.getBytes(), 0, buf, 0, len);
+            byte[] receiveData = new byte[MAX_PACKET_SIZE];
+            byte[] sendData;
 
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
 
-            InetAddress addr = packet.getAddress();
-            int srcPort = packet.getPort();
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
 
-            packet.setData(buf);
+                // Extract client's request (file name or file data)
+                String request = new String(receivePacket.getData(), 0, receivePacket.getLength());
 
-            packet.setAddress(addr);
-            packet.setPort(srcPort);
+                if (request.startsWith("GET:")) {
+                    try {
+                        // Request to retrieve file by name
+                        String fileName = request.substring(4); // Remove "GET:" prefix
 
+                        // Read requested file
+                        File file = new File(fileName);
+                        FileInputStream fileInputStream = new FileInputStream("./files/" + fileName);
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
-            socket.send(packet);
-        } catch (IOException e) {
-            System.err.println(e);
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+
+                        // Read file into buffer
+                        while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                            byteArrayOutputStream.write(buffer, 0, bytesRead);
+                        }
+
+                        // Get file data
+                        byte[] fileData = byteArrayOutputStream.toByteArray();
+
+                        // Send file data back to client
+                        DatagramPacket sendPacket = new DatagramPacket(fileData, fileData.length, clientAddress, clientPort);
+                        serverSocket.send(sendPacket);
+
+                        receiveData = new byte[MAX_PACKET_SIZE];
+                        receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                        socket.receive(receivePacket);
+
+                        // Process acknowledgment
+                        String ackMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                        System.out.println("Server acknowledgment: " + ackMessage);
+                    } catch (Exception error) {
+                        System.err.println(error);
+                    }
+                } else if (request.startsWith("PUT:")) {
+                    try {
+                        // Request to receive file data from client
+                        String fileName = request.substring(4); // Remove "PUT:" prefix
+
+                        // Receive file data from client
+                        DatagramPacket dataPacket = new DatagramPacket(receiveData, receiveData.length);
+                        serverSocket.receive(dataPacket);
+
+                        // Write received file data to file
+                        FileOutputStream fileOutputStream = new FileOutputStream("./files/" + fileName);
+                        fileOutputStream.write(dataPacket.getData(), 0, dataPacket.getLength());
+
+                        // Send acknowledgment to client
+                        String ackMessage = "File received successfully";
+                        sendData = ackMessage.getBytes();
+                        DatagramPacket ackPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+                        serverSocket.send(ackPacket);
+                        System.out.println("Server has successfully received the file: " + fileName);
+                    } catch(Exception error) {
+                        System.err.println(error);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        socket.close();
+        serverSocket.close();
     }
 
-    public static void main(String[] args) throws IOException {
-        //new UDPSocketServer().start();
-        System.out.println("UDP Socket Server");
-        Scanner input = new Scanner(System.in);
-        System.out.println("1. Read a file");
-        System.out.println("2. Write a file");
-        System.out.println("Enter a number: ");
-        String in = input.next();
-        if (Objects.equals(in, "1")) {
-            //read
-        } else if (Objects.equals(in, "2")) {
-            //write
-        } else {
-            System.out.println("Please enter a valid choice");
-        }
+    public static void main(String[] args) throws SocketException {
+        System.out.println("Server started");
+        new UDPSocketServer().start();
     }
-
 }
